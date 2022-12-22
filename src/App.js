a603af296f6ec21b79e3914b0d84f79fc4a460c3
epiglotis <83986620+epiglotis@users.noreply.github.com>
2022-12-22 14:56:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,26 +5,26 @@ import NewExpense from "./Components/NewExpense/NewExpense";
 const DUMMY_EXPENSES = [
   {
     id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
+    title: "Coffee",
+    amount: 6,
     date: new Date(2020, 7, 14),
   },
   {
     id: "e2",
-    title: "New TV",
+    title: "TV",
     amount: 799.49,
     date: new Date(2021, 2, 12),
   },
   {
     id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
+    title: "Books",
+    amount: 68.49,
     date: new Date(2021, 2, 28),
   },
   {
     id: "e4",
-    title: "New Desk (Wooden)",
-    amount: 450,
+    title: "Xiaomi TV Box",
+    amount: 56,
     date: new Date(2021, 5, 12),
   },
 ];
